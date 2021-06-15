e9ee0beae422eab910e1f7101ee3a68ed2d0462a
bokiv <borislavivanisevic@gmail.com>
2021-06-15 20:12:41 +0200

    commit 12.06

--- events/soccer/index.js
+++ events/soccer/index.js
@@ -1,5 +1,5 @@
 "use-strict"
-var player = document.querySelector("#player");
+/*var player = document.querySelector("#player");
 var bg = document.querySelector("#bg");
 var btn = document.querySelector("#button");
 var status = true;
@@ -17,4 +17,27 @@ function movePlayer(event) {
         player.style.left = posX + 'px';  
     }
 }
-bg.addEventListener("click" , movePlayer)
+bg.addEventListener("click" , movePlayer)*/
+
+var player = document.getElementById('player');
+var bg = document.getElementById('bg');
+var t = document.getElementById('t');
+var status = 'on';
+function movePlayer(eventClick) {
+    if(status == 'on') {
+        player.style.top = eventClick.pageY + 'px';
+        player.style.left = eventClick.pageX + 'px';
+        console.log(eventClick);
+    }
+    else {
+        player.style.left = 0;
+        player.style.top = 0;
+    }
+}
+bg.addEventListener('click', movePlayer);
+player.addEventListener('mouseover', function () {
+    player.title = 'Cristiano Ronaldo';
+})
+function stopA() {
+    status = 'off';
+}
