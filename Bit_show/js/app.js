a5c65bd75568157ad29f781fe0f2481c2174ae94
bokiv <borislavivanisevic@gmail.com>
2021-08-07 19:24:28 +0200

    commit 02.08.

--- Bit_show/js/app.js
+++ Bit_show/js/app.js
@@ -1,9 +1,130 @@
-var key = window.localStorage.getItem("id");
-
-request.open("GET", "http://api.tvmaze.com/shows/" + key + "/seasons");
-
-// first request for show 
-
-// second request for season 
-
-// third request for cast
+window.addEventListener('load', function() {
+    // window.localStorage.clear();
+  // TV MAZE API: /shows/:id/seasons(:id je dinamicki)
+  // primer za detalje: https://api.tvmaze.com/shows/169
+  // primer za sezone: https://api.tvmaze.com/shows/169/seasons
+  // primer za glumce: https://api.tvmaze.com/shows/169/cast
+  // primer za poseter: https://api.tvmaze.com/shows/82/images
+  // broj sezona = duzina response-a sa /shows/:id/seasons (data.length)
+  
+  var key = window.localStorage.getItem("id");  
+  const baseUrl = 'https://api.tvmaze.com';
+  let main = document.querySelector(".container");
+  let rightDiv = document.querySelector(".right");
+  let leftDiv = document.querySelector(".left");
+  let h2 = document.querySelector(".titleH2")
+  
+  
+  const requestTitle = new XMLHttpRequest();
+  requestTitle.open("GET", `${baseUrl}/shows/${key}`);
+  requestTitle.onload = function(){
+    if(requestTitle.status === 200){
+      const dataTitle = JSON.parse(requestTitle.responseText);
+      h2.textContent = dataTitle.name; 
+    }
+  }
+  requestTitle.send();
+  
+  
+  
+  const requestSeasons = new XMLHttpRequest();
+  requestSeasons.open("GET", "http://api.tvmaze.com/shows/" + key + "/seasons");
+  requestSeasons.onload = function(){
+    if (requestSeasons.status === 200){
+      const data = JSON.parse(requestSeasons.responseText);
+  
+  
+        //  ******* creating Season (number) ***** 
+        // let mainDiv = document.createElement("div");
+        let title = document.createElement("h3");
+        title.textContent = "Seasons (" + data.length + ")";
+        // main.append(mainDiv);
+        rightDiv.append(title);
+        //  ******* creating Season (number) ***** 
+  
+  
+        //  ******* creating Season LIST ***** 
+      data.forEach(function(element){
+      // let listOfSeasons = document.createElement("div");
+      let li = document.createElement("li");
+      let ul = document.createElement("ul"); 
+  
+      let seasonTime = document.createElement("h4");
+      let seasonTable = seasonTime.textContent = (element.premiereDate + " - "  + element.endDate);
+      li.textContent = seasonTable; 
+      // main.append(listOfSeasons);
+      rightDiv.append(ul);
+      ul.append(li);
+      //  ******* creating Season LIST ***** 
+      })
+    }  
+  }
+  requestSeasons.send();
+  
+  
+  
+  /***************  THE CAST ******************************** */
+  const requestCast = new XMLHttpRequest();
+  requestCast.open("GET", "https://api.tvmaze.com/shows/" + key + "/cast"); 
+  // requestCast.open("GET", `${baseUrl}/shows/${key}/cast`)
+  requestCast.onload = function(){
+    if (requestCast.status === 200){
+      const data = JSON.parse(requestCast.responseText);
+      let cast = document.createElement("h3");
+      cast.textContent = "Cast";
+      rightDiv.append(cast);
+  
+      data.forEach(function(element){
+      // let actorList = document.createElement("div");
+      let actorLi = document.createElement("li");
+      let actorUl = document.createElement("ul");
+      let actors = document.createElement("h4");
+      let actorNames = actors.textContent = element.person.name;
+      actorLi.textContent=actorNames;
+  
+      // main.append(actorList);
+      rightDiv.append(actorUl);
+      actorUl.append(actorLi);
+      })
+    }
+  }
+  requestCast.send();
+  /***************  THE CAST ******************************** */
+  
+  
+  
+  
+  /***************  SHOW DETAILS ******************************** */
+  const requestDetails = new XMLHttpRequest();    //slika
+  // requestDetails.open("GET", "http://api.tvmaze.com/shows/" + key);
+  requestDetails.open("GET", `${baseUrl}/shows/${key}`)
+  requestDetails.onload = function(){
+    if (requestDetails.status === 200){
+      const data = JSON.parse(requestDetails.responseText);
+      let show = document.createElement("h3");
+      show.textContent = "Show details";
+      let divParagraph = document.createElement("div");
+      divParagraph.setAttribute("class", "details");
+      main.append(show);
+      main.append(divParagraph);
+      divParagraph.innerHTML=data.summary;   
+    }
+  }
+  requestDetails.send();
+  
+  /***************  POSTER ******************************** */
+  const requestPoster = new XMLHttpRequest();
+  requestPoster.open("GET", `${baseUrl}/shows/${key}/images`)
+  requestPoster.onload = function(){
+    if (requestPoster.status === 200){
+      const posterData = JSON.parse(requestPoster.responseText);
+      const img = document.createElement("img");
+      img.setAttribute("src", posterData[0].resolutions.original.url);
+      img.classList.add("image-size");
+      leftDiv.append(img);
+    }
+  }
+  requestPoster.send();
+  
+  
+  });
--- Bit_show/js/main.js
+++ Bit_show/js/main.js
@@ -1,45 +1,48 @@
-var div = document.querySelector(".container");
-var main = document.querySelector("main");
-var row = document.querySelector("row")
+const baseUrl =  "http://api.tvmaze.com/shows";
 
-var shows = [];
+let main = document.querySelector(".row");
 
-var request = new XMLHttpRequest();
+let top50 = [];
 
-request.open("GET", "http://api.tvmaze.com/shows");
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
+
+const request = new XMLHttpRequest();
+
+request.open("GET", `${baseUrl}`, true);
 
 request.onload = function () {
   if (request.status === 200) {
-    var response = request.responseText;
-    var data = JSON.parse(response);
-
-    //  shows = data.map(function (element) {
-    //   return element;
-    // });
+    let response = request.responseText;
+    const data = JSON.parse(response);
 
     data.forEach(function (el) {
-      shows.push(el);
+      data.push(el);
     });
 
-    var sortedShows = shows.sort(function (a, b) {
+    let sortedShows = data.sort(function (a, b) {
       return b.rating.average - a.rating.average;
     });
-    var top50 = sortedShows.slice(0, 50);
+    let top50 = sortedShows.slice(0, 50);
 
     top50.forEach(function (el) {
 
-      var mainDiv = document.createElement("div");
-      var div = document.innerHtml("mainDiv")
-      div.setAttribute("data-our-key" ,el.id);
-      div.classList = "col-3 tv-div"
-      var img = document.createElement("img");
+      let mainDiv = document.createElement("div");
+      mainDiv.setAttribute("data-our-key" ,el.id);
+      mainDiv.classList = "tv-div col-md-4"
+      let img = document.createElement("img");
       img.setAttribute("src" , el.image.medium);
-      var title = document.createElement("h4");
+      let title = document.createElement("h4");
       title.textContent = el.name;
       main.append(mainDiv);
       mainDiv.append(img);
       mainDiv.append(title);
     });
+
+
     
 
     window.localStorage.clear();
@@ -48,7 +51,7 @@ request.onload = function () {
     div.forEach(function(element){
       element.addEventListener("click", function(event){
         //  event.preventDefault();
-        let key = document.querySelector(".tv-div").getAttribute("data-our-key");
+        let key = element.getAttribute("data-our-key");
         window.localStorage.setItem("id", key);
         document.location = "second.html"
       });
@@ -62,3 +65,64 @@ request.onload = function () {
 
 request.send();
 
+//Search field//
+let ul = null;
+let $searchShow = "http://api.tvmaze.com/search/shows?q=";
+
+ const btnSearch = document.getElementById('btnSearch');
+ btnSearch.addEventListener('keyup', function(event) {
+   console.log(event)
+   if (!event.target.value) {
+     return;
+   }
+   const requestSearch = new XMLHttpRequest();
+   requestSearch.open("GET", `${$searchShow}:${event.target.value}`, true);
+   requestSearch.onload = function(){
+     if (requestSearch.status === 200){
+       const searchResultsWrapper = document.querySelector(".search-results");
+       console.log({searchResultsWrapper});
+       if (searchResultsWrapper && searchResultsWrapper.children.length > 0) {
+         console.log('VEC POSTOJE REZULTATI PRETRAGE');
+         removeAllChildNodes(searchResultsWrapper);
+       }
+
+       const searchResults = JSON.parse(requestSearch.responseText);
+       console.log({searchResults})
+
+      //  if (!searchResultsWrapper) {
+      //     ul = document.createElement("li");
+      //     ul.classList.add('search-results');
+      //     ul.append(li);
+      //   }
+            
+       searchResults.forEach(function(element){
+          console.log(element);
+          let text = document.createElement("h5").textContent;
+          //let fullText= text.textContent=element.show.name
+        
+          let li = document.createElement("li"); 
+          li.classList.add("single-result");
+          li.innerText= element.show.name;
+          li.setAttribute("data-our-key", element.show.id);
+          ul.append(li);
+       })  
+    
+     }
+   }
+
+
+
+   requestSearch.send();
+ });
+ //END SEARCH INPUT FIELD//
+ document.addEventListener("click", function (event) {
+  //event Delegation
+  console.log(event.target);
+  if (event.target.classList.contains("single-result")) {
+    const id = event.target.getAttribute("data-our-key");
+    console.log(id);
+
+    window.localStorage.setItem("id", id);
+    document.location = "second.html";
+  }
+});
